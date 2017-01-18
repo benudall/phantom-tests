41fd221a7dca77135f1d42b77515ed58d5a6feac
verefor <verefor@googlemail.com>
2017-01-18 16:47:58 +0000

    added find a dealer functionality

--- SuzukiSearch.js
+++ SuzukiSearch.js
@@ -1,26 +1,59 @@
 var page = require('webpage').create();
 
-machines=[];
-function check(){
-	page.open("https://cars.suzuki.co.uk/search/?searchSuzuki=ignis", function(status) {
-			var machine = page.evaluate(function() {
-				comments=document.head.childNodes;
-				for(x=0;x<comments.length;x++){
-					if(comments[x].nodeType==Node.COMMENT_NODE && comments[x].data.indexOf("Machine")>-1){
-						return comments[x].data+" has "+document.querySelector(".search-counter").innerText.split(" for")[0];
-					}
+searchmachines=[];
+fadmachines=[];
+page.onError=function(msg,trace){}
+function search(){
+	page.open("https://cars.suzuki.co.uk/search/?searchSuzuki=ignis",function(status){
+		var machine = page.evaluate(function(){
+			comments=document.head.childNodes;
+			for(x=0;x<comments.length;x++){
+				if(comments[x].nodeType==Node.COMMENT_NODE && comments[x].data.indexOf("Machine")>-1){
+					return comments[x].data+"returns "+document.querySelector(".search-counter").innerText.split(" for")[0];
 				}
-			});
-			if(machines.indexOf(machine)==-1){
-				machines.push(machine);
-				console.log(machine);
 			}
-			if(machines.length==4){
-				phantom.exit();
-			}
-			else{
-				check();
-			}
+		});
+		if(searchmachines.indexOf(machine)==-1){
+			searchmachines.push(machine);
+			console.log(machine);
+		}
+		if(searchmachines.length==4){
+			console.log("Searching Find a Dealer for 'WC1X8HN'");
+			findadealer();
+		}
+		else{
+			search();
+		}
 	});
 }
-check();
+function findadealer(){
+	page.open("https://cars.suzuki.co.uk/find-a-dealer?PostcodeForDealers=WC1X8HN",function(status){
+		setTimeout(function(){
+			var machine = page.evaluate(function(){
+					comments=document.head.childNodes;
+					for(x=0;x<comments.length;x++){
+						if(comments[x].nodeType==Node.COMMENT_NODE && comments[x].data.indexOf("Machine")>-1){
+							if(document.querySelector(".address__header")){
+								return comments[x].data+"returns "+document.querySelector(".address__header").innerText;
+							}
+							else{
+								return "not found"+Date.now();
+							}
+						}
+					}
+				});
+				if(fadmachines.indexOf(machine)==-1){
+					fadmachines.push(machine);
+					console.log(machine);
+				}
+				if(fadmachines.length==4){
+					phantom.exit();
+				}
+				else{
+					findadealer();
+				}
+		},3000);
+	});
+}
+console.log("Searching site for 'Ignis'");
+search();
