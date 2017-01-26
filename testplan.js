54f87e121038de30c765224250d13e164fbc853d
verefor <verefor@googlemail.com>
2017-01-26 17:44:11 +0000

    got working trigger when all tests are done

--- testplan.js
+++ testplan.js
@@ -1,23 +1,33 @@
-myobj={}
+myobj={tests:{},results:{}};
 function instance(testname,starturl,func){
-	myobj[testname]={};
-	console.log("test built");
-	myobj[testname].page=require('webpage').create();
-	console.log("test page built");
-	myobj[testname].page.open(starturl,func);
-	console.log("test ran");
+	myobj.tests[testname]={};
+	myobj.tests[testname].page=require('webpage').create();
+	myobj.tests[testname].page.open(starturl,func);
+	myobj.tests[testname].page.end=function(result){
+		this.close();
+		myobj.results[testname]={};
+		myobj.results[testname].result=result;
+		if(Object.keys(myobj.results).length==Object.keys(myobj.tests).length){
+			console.log("All tests done");
+		}
+		
+	}
 }
-console.log("Instance defined");
 
 instance("Test1","https://google.com",function(){
 	this.render('google.png');
 	console.log("google.png");
+	this.end("Pass");
 });
 
 instance("Test2","https://wikipedia.org",function(){
 	this.render('wiki.png');
 	console.log("wiki.png");
+	this.end("Pass");
 });
 
-
-console.log("END")
+instance("Test3","https://yahoo.com",function(){
+	this.render('yahoo.png');
+	console.log("yahoo.png");
+	this.end("Pass");
+});
