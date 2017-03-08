b225a52f97d163f65db99a132acaa3f4b12b4995
verefor <verefor@googlemail.com>
2017-03-08 16:59:27 +0000

    moved single.js to it's own folder and added screenshot rendering

--- single/single.js
+++ single/single.js
@@ -1,7 +1,7 @@
 var page = require('webpage').create();
 
 page.onError = function(msg, trace){}
-
+page.viewportSize={width:1680,height:720};
 start=Date.now();
 machines=[];
 console.log("");
@@ -9,12 +9,14 @@ console.log(Date());
 console.log("");
 		
 function search(){
-	page.open("https://prod-suzuki-staging.azurewebsites.net/" , function(status){
+	page.open("https://cars.suzuki.co.uk/dealers/progress-suzuki-milton-keynes/?distance=6" , function(status){
 		var machine = page.evaluate(function(){
+			document.body.className+=" no-animate";
 			return Array.prototype.slice.call(document.head.childNodes).filter(function(x){return (x.nodeName=='#comment' && x.data.match('Machine'))})[0].data;
 		});
 		if(machines.indexOf(machine) == -1){
 			machines.push(machine);
+			page.render(machine.split(" ")[2]+".png");
 			console.log(machine);
 		}
 		if(machines.length == 4){
