bf85564620adf676b20cf628ee48d87833f63294
verefor <verefor@googlemail.com>
2017-03-22 12:08:17 +0000

    added in starturl recording and notes

--- testplan/testplan.js
+++ testplan/testplan.js
@@ -1,23 +1,24 @@
 fs = require('fs');
 tests={}; //Defining wrapper for test data
 function instance(testname,starturl,func){ //Container function for creating a test
-	tests[testname]={};
-	tests[testname].page=require('webpage').create();
-	tests[testname].page.viewportSize={width:1680,height:720};
-	tests[testname].page.testname=testname;
+	tests[testname]={}; //Creates test
+	tests[testname].starturl=starturl; //Records starting url
+	tests[testname].page=require('webpage').create(); //Creates page for test
+	tests[testname].page.viewportSize={width:1680,height:720}; //Sets page size
+	tests[testname].page.testname=testname; //Sets testname within page for easier access later
 	tests[testname].page.renderelement=function(element,filename){ //Function to screenshot an element
-		var oldclip = this.clipRect
-		this.clipRect = this.evaluate(function(element){
+		var oldclip = this.clipRect //Saves original screenshot boundries
+		this.clipRect = this.evaluate(function(element){ //Gets boundries of element
 			return document.querySelector(element).getBoundingClientRect();
 		},element);
-		this.render(filename);
-		this.clipRect = oldclip;
+		this.render(filename); //Screenshots element
+		this.clipRect = oldclip; //Reverts screenshot boundries to original
 	};
 	tests[testname].page.end=function(result){
 		this.close(); //Closes the page to save memory
 		delete tests[testname].page; //Removes page data from JSON to save space
-		tests[testname].result=result;
-		if(Object.keys(tests).length==Object.keys(tests).filter(function(x){return(tests[x].result!=undefined? true: false)}).length){
+		tests[testname].result=result; //Records result to JSON
+		if(Object.keys(tests).length==Object.keys(tests).filter(function(x){return(tests[x].result!=undefined? true: false)}).length){ //If number of results matches number of tests
 			console.log("All tests done");
 			for(x in tests){
 				console.log(x+" "+tests[x].result);
